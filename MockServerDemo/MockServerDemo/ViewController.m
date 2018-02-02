c3b226099f3cfe9b5686d668dc9a755501c34be7
肖扬 <xy_dtb@163.com>
2018-02-02 13:42:04 +0800

    上传文件

--- MockServerDemo/MockServerDemo/ViewController.m
+++ MockServerDemo/MockServerDemo/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -16,7 +19,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.textField.text = @"http://www.apiopen.top:88/createStatisticsKey?appId=com.chat.peakchao&passWd=123456";
+}
+
+- (IBAction)requestData:(id)sender {
+    
+    __weak typeof(self) weakSelf = self;
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURL *url = [NSURL URLWithString:self.textField.text];
+    NSURLSessionTask *task = [session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError* error) {
+        // 默认是子线程.
+        dispatch_async(dispatch_get_main_queue(), ^{
+            weakSelf.textView.text = [NSString stringWithFormat:@"返回数据：\n%@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]];
+        });
+    }];
+    // 开启任务
+    [task resume];
 }
 
 
